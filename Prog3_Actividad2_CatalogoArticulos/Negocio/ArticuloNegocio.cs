460afc28593d389a840e808870e51864e57a912e
Sofia Bernigola <smbernigola@gmail.com>
2025-09-09 09:30:09 -0300

    Update ArticuloNegocio Consulta DB Listar

--- Prog3_Actividad2_CatalogoArticulos/Negocio/ArticuloNegocio.cs
+++ Prog3_Actividad2_CatalogoArticulos/Negocio/ArticuloNegocio.cs
@@ -17,7 +17,35 @@ namespace negocio
 
             try
             {
-                
+                datos.setearConsulta("SELECT A.Id, A.Codigo, A.Nombre, A.Descripcion, A.IdMarca, A.IdCategoria, A.Precio, M.Id, M.Descripcion, C.Id, C.Descripcion, I.ImagenUrl, I.Id FROM ARTICULOS A, MARCAS M, CATEGORIAS C, IMAGENES I WHERE A.IdMarca = M.Id AND A.IdCategoria = C.Id AND A.Id = I.IdArticulo");    
+                datos.ejecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Articulo aux = new Articulo();
+                    aux.Id = (int)datos.Lector["A.Id"];
+                    aux.Codigo = (string)datos.Lector["A.Codigo"];
+                    aux.Nombre = (string)datos.Lector["A.Nombre"];
+                    aux.Descripcion = (string)datos.Lector["A.Descripcion"];
+                    aux.Precio = (decimal)datos.Lector["A.Precio"];
+
+                    aux.Imagenes = new List<Imagen>();
+                    Imagen img = new Imagen();
+                    img.Id = (int)datos.Lector["I.Id"];
+                    img.Url = (string)datos.Lector["I.ImagenUrl"];
+                    img.ArticuloId = aux.Id;
+                    aux.Imagenes.Add(img);
+
+                    aux.Marca = new Marca();
+                    aux.Marca.Id = (int)datos.Lector["M.Id"];
+                    aux.Marca.Descripcion = (string)datos.Lector["M.Descripcion"];
+
+                    aux.Categoria = new Categoria();
+                    aux.Categoria.Id = (int)datos.Lector["C.Id"];
+                    aux.Categoria.Descripcion = (string)datos.Lector["C.Descripcion"];
+
+                    lista.Add(aux);
+                }
             }
             catch (Exception ex)
             {
