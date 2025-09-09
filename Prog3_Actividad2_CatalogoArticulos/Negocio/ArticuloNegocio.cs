6a6227cdc75bd73a3ed3760a992acdbcfa7bf06c
Sofia Bernigola <smbernigola@gmail.com>
2025-09-08 22:26:56 -0300

    Agrego la clase ArticuloNegocio y hago publicos las clases de dominio

---
+++ Prog3_Actividad2_CatalogoArticulos/Negocio/ArticuloNegocio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+namespace negocio
+{
+    internal class ArticuloNegocio
+    {
+        private AccesoDatos datos = new AccesoDatos();
+
+        public List<Articulo> listar()
+        {
+            List<Articulo> lista = new List<Articulo>();
+
+            return lista;
+        }
+    }
+}
--- Prog3_Actividad2_CatalogoArticulos/dominio/Imagen.cs
+++ Prog3_Actividad2_CatalogoArticulos/dominio/Imagen.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace dominio
 {
-    internal class Imagen
+    public class Imagen
     {
         public int Id { get; set; }
         public string Url { get; set; }
